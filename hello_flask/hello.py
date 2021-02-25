6a8cb08eb4fa441f7cd2b72fa0e8d6e3262d53e3
Jerome Johnson <jerome.j.johnson@outlook.com>
2021-02-25 12:38:40 -0500

    created server.py

--- hello_flask/hello.py
+++ hello_flask/hello.py
@@ -3,6 +3,19 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html", phrase="hello", times=5)	# notice the 2 new named arguments!
+@app.route('/success')
+def success():
+    return 'success'
+@app.route('/hello/<name>') # for a route '/hello/____' anything after '/hello/' gets passed as a variable 'name'
+def hello(name):
+    print(name)
+    return "Hello, " + name
+@app.route('/users/<username>/<id>') # for a route '/users/____/____', two parameters in the url get passed as username and id
+def show_user_profile(username, id):
+    print(username)
+    print(id)
+    return "Username: " + username + ", ID: " + id
+
 if __name__=="__main__":
     app.run(debug=True)
                 
