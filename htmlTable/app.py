f360c5e6b3f212d185d35218f6c6719a2b6011f7
Jerome Johnson <jerome.j.johnson@outlook.com>
2021-03-09 16:50:45 -0500

    created table with bootstrap framework

--- htmlTable/app.py
+++ htmlTable/app.py
@@ -1,11 +1,19 @@
-import sys
+from flask_bootstrap import Bootstrap
 from flask import Flask, render_template
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-    return render_template("index.html", phrase="hello", times=5)
+    userList = [
+    {'first_name' : 'Michael', 'last_name' : 'Choi'},
+    {'first_name' : 'John', 'last_name' : 'Supsupin'},
+    {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+    {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+
+    return render_template("index.html", phrase="User List", users=userList, num=0)
 
 if __name__=="__main__":
     app.run(debug=True)
