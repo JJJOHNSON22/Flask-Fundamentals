035d68f4195e0cd4c3cf41b7a69d62193925869b
Jerome Johnson <jerome.j.johnson@outlook.com>
2021-03-09 08:40:55 -0500

    created a row of responsive squares

--- checkerboard/app.py
+++ checkerboard/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 
 def index():
-    return render_template("index.html", phrase="home")
+    return render_template("index.html", phrase="Home")
 
 if __name__=="__main__":
     app.run(debug=True)
