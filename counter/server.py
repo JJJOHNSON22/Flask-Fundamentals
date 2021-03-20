81852c0be6261ce7573c77a57be78aca06b7f8ba
Jerome Johnson <jerome.j.johnson@outlook.com>
2021-03-19 23:50:25 -0400

    finished counter

--- counter/server.py
+++ counter/server.py
@@ -1,4 +1,5 @@
 from typing import Counter
+import base64
 from flask_bootstrap import Bootstrap
 from flask import Flask, render_template, request, redirect, session 
 
@@ -9,17 +10,43 @@ bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-    if 'count' in session:
-        session['count'] = int(session['count']) + 1
-        print(f"Count is {session['count']}")
+    session['counter'] = 1
+    print('*'*80)
+    print(session['counter'])
+    if 'count' in session and 'countCustom' in session:
+        if int(session['countCustom']) > 1:
+            session['count'] = int(session['count']) + int(session['countCustom'])
+            print(f"Count is {session['count']}")
+        else:
+            session['count'] = int(session['count']) + int(session['counter'])
     else:
         print("First time visit")
         session['count'] = 1
+        session['countCustom'] = 0
+        session['refreshCount'] = 0
+    session['refreshCount'] = int(session['refreshCount']) + int(session['counter'])
     return render_template("index.html", phrase="Count", tempCount=session['count'])
 
-@app.route('/destroy')
+@app.route('/destroy', methods=['POST'])
 def destroy_session():
-    session.pop['count']
+    session.clear()
+    return redirect('/')
+
+@app.route('/add_two', methods=['POST'])
+def addTwo():
+    if int(session['countCustom']) > 1:
+        session['count'] = int(session['count']) + int(session['countCustom'])
+        print(f"Count is {session['count']}")
+    else:
+        session['count'] = int(session['count']) + int(session['counter'])
+        print(f"Count is {session['count']}")
+    return redirect('/')
+
+@app.route('/custom', methods=['POST'])
+def customCounter():
+    session['countCustom'] = request.form['customCount']
+    print(session['countCustom'])
+    return redirect('/')
 
 if __name__=="__main__":
     app.run(debug=True)
