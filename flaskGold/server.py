aba24f72b893c74d35a56ebbebce284a92d8a942
Jerome Johnson <jerome.j.johnson@outlook.com>
2021-03-26 00:51:55 -0400

    finished message box and victory condition

--- flaskGold/server.py
+++ flaskGold/server.py
@@ -8,19 +8,39 @@ bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
+    session['earnGold'] = [
+        {'jobName':'Farm', 'payRate':'1', 'earnings':'10-20'},
+        {'jobName':'Cave', 'payRate':'2', 'earnings':'5-10'},
+        {'jobName':'House', 'payRate':'4', 'earnings':'2-5'},
+        {'jobName':'Casino', 'payRate':'casino', 'earnings':'or lose 0-50'},
+        ]
     if 'gold' not in session:
         session['gold'] = 10
+        session['messages'] = []
+        session['counter'] = 0
+    else:
+        if int(session['gold']) >= 50:
+            print('*'*80)
+            print('You Won!')
     return render_template("index.html")
 
 @app.route('/process', methods=['POST'])
 def processMoney():
+    messageList = session['messages']
+    name = request.form['job']
     if request.form['earn'] == 'casino':
-        randEarn = random.randint(0,100) - 50
-        session['gold'] = int(session['gold'] + randEarn)
+        total = random.randint(0,100) - 50
+        session['gold'] = int(session['gold'] + total)
+        if total < 0:
+            messageList.insert(0, f'<li style="list-style: none; text-align:left; color: red;"> You lost ${total} from the {name}. </li>')
+        else:
+            messageList.insert(0, f'<li style="list-style: none; text-align:left; color: green;"> You won ${total} from the {name}! </li>')
     else:
         randEarn = random.randint(10,20)
         total = randEarn // int(request.form['earn'])
         session['gold'] = int(session['gold'] + total)
+        messageList.insert(0, f'<li style="list-style: none; text-align:left; color: green;"> You earned ${total} from {name}. </li>')
+    session['messages'] = messageList
     return redirect('/')
 
 @app.route('/reset', methods=['POST'])
